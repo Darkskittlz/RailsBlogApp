afbcdcd6b70588f88b6b175c30527f424eb5e36c
Darkskittlz (Linux) <darkskiiittles@gmail.com>
2024-02-21 14:20:59 -0500

    continuing tutorial

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,7 @@
+class AboutController < ApplicationController
+# Here I am defining a method called index. It doesn't need to do anything at all because I am inheriting from the application controller. class AboutController < ApplicationController is saying if AboutController does anything then lets do that, but otherwise lets use the default rails renderer. The default rails renderer will go into the 
+
+  def index
+
+  end
+end
